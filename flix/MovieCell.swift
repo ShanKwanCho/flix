de0e5aa4b40f156cf3b7f120772d5265663c450e
ShanKwanCho <scho4@mail.sfsu.edu>
2018-09-05 23:36:22 -0700

    commit messge

---
+++ flix/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by ShanCho on 9/5/18.
+//  Copyright © 2018 ShanCho. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
